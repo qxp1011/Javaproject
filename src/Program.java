34202548bea2461bef39fe5628bd8410087982b2
邱小平 <qxp@xiaomutech.com>
2016-04-28 16:09:36 +0800

    commit 3

--- src/Program.java
+++ src/Program.java
@@ -1,13 +1,49 @@
 /**
  * Created by XMqxp on 2016/4/28.
  */
-public class Program {
+
+import com.xm.GUI.javagui;
+import com.xm.SocketIO.nettyserver;
+import com.xm.buffer.javabuffer;
+import com.xm.control.javacontrol;
+import com.xm.db.javadb;
+import com.xm.thread.javathread;
+
+public final class Program {
+
+   // public static boolean b_Exit=true;
 
     public static void main(String[] args) throws Exception {
 
+        System.out.print("Program Start!\n");
 
-        System.out.print("Program Exit\n");
 
+        //定义局部变量
+        javagui m_javagui = new javagui();
+        javacontrol m_javacontrol = new javacontrol();
+        javathread m_javathread = new javathread();
+        nettyserver m_nettyserver = new nettyserver();
+        javabuffer m_javabuffer = new javabuffer();
+        javadb m_javadb = new javadb();
+
+        //调用线程类的内部函数
+        m_javathread.Test();
+
+        //启动线程，开始异步操作
+        m_javathread.start();
+
+        //等待线程退出标识
+        while (javathread.b_Exit) {
+
+            //同时还可以做其它事情
+            System.out.print("Program is Running!\n");
+
+            //这里休眠一下
+            Thread.sleep(1000);
+        }
+
+        //退出进程
+        System.out.print("Program Exit!\n");
     }
 
 }
--- src/com/xm/thread/javathread.java
+++ src/com/xm/thread/javathread.java
@@ -1,7 +1,39 @@
 package com.xm.thread;
 
+
 /**
  * Created by XMqxp on 2016/4/28.
  */
-public class javathread {
+public class javathread extends Thread    {
+
+    //全局变量，标识线程退出
+    public static boolean b_Exit=true;
+
+    //线程类的内部普通函数
+    public void Test(){
+
+        System.out.print("javathread.Test is running!\n");
+
+    }
+
+    //继承Thread类之后必须实现的函数
+    public void run()
+    {
+        try {
+
+            System.out.print("javathread.run is running!\n");
+
+            //休眠10秒
+            Thread.sleep(10000);
+
+            System.out.print("Thread is Exit!;");
+
+            //本线程退出了
+            b_Exit=false;
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
 }

6332eceffea7e97c43e3c1994bddd5b210ae165e
qxp <qxp@xiaomutech.com>
2016-05-24 09:17:51 +0800

    JAVA基本数据类型1

---
+++ src/BaseDataType.java
@@ -0,0 +1,23 @@
+/**
+ * Created by 小平 on 2016/5/23.
+ */
+public class BaseDataType {
+
+    //JAVA的基本数据类型测试
+
+
+    public static void main(String[] args) throws Exception
+    {
+        int i=0;//整数
+        long l=0l;//长整数
+        float f=0.0f;//单精度
+        double d=0.0d;//双精度
+
+        byte b=0x00;
+        char c=0x00;
+
+        System.out.println("i="+i+"\n");
+
+    }
+
+}
